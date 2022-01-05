015e3fdee4303a8d47baf70548e4c1255ceb3e89
AishaD <AishaDahir75@gmail.com>
2022-01-05 13:57:00 +0000

    update to 1.3.1 rpt workflow

--- egg5_config.py
+++ egg5_config.py
@@ -87,7 +87,7 @@ vcf2xls_stage_id = "stage-Fyq5ypj433GzxPK360B8Qfg5"
 generate_bed_stage_id = "stage-Fyq5yy0433GXxz691bKyvjPJ"
 athena_stage_id = "stage-Fyq5z18433GfYZbp3vX1KqjB"
 
-rpt_workflow_id = "{}:workflow-G7BjXJ8433GXB1GV52p4qQy1".format(ref_project_id)
+rpt_workflow_id = "{}:workflow-G7BkqVQ433GYj4J1KzJ5gQzQ".format(ref_project_id)
 
 rpt_stage_input_dict = {
     # vcf2xls
