700cf32c6667a8189d619eff8577b69f8d1f5947
sophie22 <rzsozso@gmail.com>
2023-04-27 11:49:08 +0100

    merge and update reports and reanalysis inputs

--- egg5_config_v2.0.0.py
+++ egg5_config_v2.0.0.py
@@ -51,7 +51,9 @@ cnvcalling_input_dict = {
 }
 
 
-# Reports
+# dias_reports
+# v2.0.4
+rpt_workflow_id = "{}:workflow-GBQ985Q433GYJjv0379PJqqg".format(ref_project_id)
 
 generate_bed_vep_stage_id = "stage-G9P8p104vyJJGy6y86FQBxkv"
 vep_stage_id = "stage-G9Q0jzQ4vyJ3x37X4KBKXZ5v"
@@ -59,91 +61,43 @@ generate_workbook_stage_id = "stage-G9P8VQj4vyJBJ0kg50vzVPxY"
 generate_bed_athena_stage_id = "stage-Fyq5yy0433GXxz691bKyvjPJ"
 athena_stage_id = "stage-Fyq5z18433GfYZbp3vX1KqjB"
 
-rpt_workflow_id = "{}:workflow-GBQ985Q433GYJjv0379PJqqg".format(ref_project_id)
-
-rpt_stage_input_dict = {
-    # generate_bed
-    "{}.sample_file".format(generate_bed_athena_stage_id): {
-        "app": "mosdepth", "subdir": "",
-        "pattern": "-E '{}(.*).per-base.bed.gz.csi$'"
-    },
-    "{}.sample_file".format(generate_bed_vep_stage_id): {
-        "app": "mosdepth", "subdir": "",
-        "pattern": "-E '{}(.*).per-base.bed.gz.csi$'"
-    },
-    # vep
-    "{}.vcf".format(vep_stage_id): {
-        "app": "sentieon-dnaseq", "subdir": "",
-        "pattern": "-E '{}(.*)[^g].vcf.gz$'"
-    },
-    # athena
-    "{}.mosdepth_files".format(athena_stage_id): {
-        "app": "mosdepth", "subdir": "",
-        # athena requires both per-base files and reference files
-        "pattern": "-E '{}(.*)(per-base.bed.gz$|reference)'"
-    },
-}
-
 rpt_dynamic_files = {
     # inputs for generate bed for vep
-    "{}.exons_nirvana ID".format(generate_bed_vep_stage_id): cds_file,
+    "{}.exons_nirvana ID".format(generate_bed_vep_stage_id): exons_nirvana,
     "{}.exons_nirvana".format(generate_bed_vep_stage_id): "",
     "{}.nirvana_genes2transcripts ID".format(generate_bed_vep_stage_id): genes2transcripts,
     "{}.nirvana_genes2transcripts".format(generate_bed_vep_stage_id): "",
     "{}.gene_panels ID".format(generate_bed_vep_stage_id): genepanels_file,
     "{}.gene_panels".format(generate_bed_vep_stage_id): "",
-    "{}.manifest ID".format(generate_bed_vep_stage_id): bioinformatic_manifest,
-    "{}.manifest".format(generate_bed_vep_stage_id): "",
+    # input for eggd_vep
+    "{}.config_file ID".format(vep_stage_id): vep_config,
+    "{}.config_file".format(vep_stage_id): "",
     # inputs for generate bed for athena
-    "{}.exons_nirvana ID".format(generate_bed_athena_stage_id): cds_file,
+    "{}.exons_nirvana ID".format(generate_bed_athena_stage_id): exons_nirvana,
     "{}.exons_nirvana".format(generate_bed_athena_stage_id): "",
     "{}.nirvana_genes2transcripts ID".format(generate_bed_athena_stage_id): genes2transcripts,
     "{}.nirvana_genes2transcripts".format(generate_bed_athena_stage_id): "",
     "{}.gene_panels ID".format(generate_bed_athena_stage_id): genepanels_file,
     "{}.gene_panels".format(generate_bed_athena_stage_id): "",
-    "{}.manifest ID".format(generate_bed_athena_stage_id): bioinformatic_manifest,
-    "{}.manifest".format(generate_bed_athena_stage_id): "",
     # inputs for athena
-    "{}.exons_file ID".format(athena_stage_id): cds_file_for_athena,
+    "{}.exons_file ID".format(athena_stage_id): exons_file,
     "{}.exons_file".format(athena_stage_id): ""
 }
 
-# reanalysis
-
-rea_stage_input_dict = {
-    # vep
+# Sample-specific input files and their search patterns
+rpt_stage_input_dict = {
+    # eggd_vep
     "{}.vcf".format(vep_stage_id): {
         "app": "sentieon-dnaseq", "subdir": "",
         "pattern": "-E '{}(.*)[^g].vcf.gz$'"
     },
-    # athena
+    # eggd_athena
     "{}.mosdepth_files".format(athena_stage_id): {
         "app": "mosdepth", "subdir": "",
         # athena requires both per-base files and reference files
         "pattern": "-E '{}(.*)(per-base.bed.gz$|reference)'"
     },
 }
-
-rea_dynamic_files = {
-    # inputs for generate bed for vep
-    "{}.exons_nirvana ID".format(generate_bed_vep_stage_id): cds_file,
-    "{}.exons_nirvana".format(generate_bed_vep_stage_id): "",
-    "{}.nirvana_genes2transcripts ID".format(generate_bed_vep_stage_id): genes2transcripts,
-    "{}.nirvana_genes2transcripts".format(generate_bed_vep_stage_id): "",
-    "{}.gene_panels ID".format(generate_bed_vep_stage_id): genepanels_file,
-    "{}.gene_panels".format(generate_bed_vep_stage_id): "",
-    # inputs for generate bed for athena
-    "{}.exons_nirvana ID".format(generate_bed_athena_stage_id): cds_file,
-    "{}.exons_nirvana".format(generate_bed_athena_stage_id): "",
-    "{}.nirvana_genes2transcripts ID".format(generate_bed_athena_stage_id): genes2transcripts,
-    "{}.nirvana_genes2transcripts".format(generate_bed_athena_stage_id): "",
-    "{}.gene_panels ID".format(generate_bed_athena_stage_id): genepanels_file,
-    "{}.gene_panels".format(generate_bed_athena_stage_id): "",
-    # inputs for athena
-    "{}.exons_file ID".format(athena_stage_id): cds_file_for_athena,
-    "{}.exons_file".format(athena_stage_id): ""
-}
-
 
 # CNV Reports
 
