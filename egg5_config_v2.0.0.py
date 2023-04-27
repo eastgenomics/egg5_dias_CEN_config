e104a3211b03a8a672f33b6792b33dcbedb4e002
sophie22 <rzsozso@gmail.com>
2023-04-27 11:49:48 +0100

    merge and update cnvreports and cnvreanalysis inputs

--- egg5_config_v2.0.0.py
+++ egg5_config_v2.0.0.py
@@ -99,76 +99,50 @@ rpt_stage_input_dict = {
     },
 }
 
-# CNV Reports
-
+# dias_cnvreports
+# vx.y.z
 cnv_rpt_workflow_id =  "{}:workflow-GJk5VXQ433GbPgPY4gGY262z".format(ref_project_id)
 
 cnv_generate_bed_excluded_stage_id = "stage-GFZQB7Q4qq8X6yjKG2pFQ58x"
-cnv_generate_bed_vep_stage_id = "stage-GG39Gq04qq8ZkfgV31yQy93v"
 cnv_annotate_excluded_regions_stage_id = "stage-GG1qYz84qq8yKzF1J2X48q62"
+cnv_generate_bed_vep_stage_id = "stage-GG39Gq04qq8ZkfgV31yQy93v"
 cnv_vep_stage_id = "stage-GFYvJF04qq8VKgq34j30pZZ3"
 cnv_generate_workbook_stage_id = "stage-GFfYY9j4qq8ZxpFpP8zKG7G0"
 
-cnv_rpt_stage_input_dict = {
-    # generate_bed for vep generate bed
-    "{}.sample_file".format(cnv_generate_bed_vep_stage_id): {
-        "app": "mosdepth", "subdir": "",
-        "pattern": "-E '{}(.*).per-base.bed.gz.csi$'"
-    },
-    # generate_bed for excluded generate bed
-    "{}.sample_file".format(cnv_generate_bed_excluded_stage_id): {
-        "app": "mosdepth", "subdir": "",
-        "pattern": "-E '{}(.*).per-base.bed.gz.csi$'"
-    },
-    # vep
-    # subdirectories always require the backward dash
-    "{}.vcf".format(cnv_vep_stage_id): {
-        "app": "eggd_GATKgCNV_call", "subdir": "CNV_vcfs/",
-        "pattern": "-E '{}(.*)_segments.vcf$'"
-    },
-    # excluded_annotate
-    # subdirectories always require the backward dash
-    "{}.excluded_regions".format(cnv_annotate_excluded_regions_stage_id): {
-        "app": "eggd_GATKgCNV_call", "subdir": "CNV_summary/",
-        "pattern": "-E '(.*)_excluded_intervals.bed$'"
-    },
-}
 
 cnv_rpt_dynamic_files = {
     # inputs for generate bed for vep
-    "{}.exons_nirvana ID".format(cnv_generate_bed_vep_stage_id): cds_file,
+    "{}.exons_nirvana ID".format(cnv_generate_bed_vep_stage_id): exons_nirvana,
     "{}.exons_nirvana".format(cnv_generate_bed_vep_stage_id): "",
     "{}.nirvana_genes2transcripts ID".format(cnv_generate_bed_vep_stage_id): genes2transcripts,
     "{}.nirvana_genes2transcripts".format(cnv_generate_bed_vep_stage_id): "",
     "{}.gene_panels ID".format(cnv_generate_bed_vep_stage_id): genepanels_file,
     "{}.gene_panels".format(cnv_generate_bed_vep_stage_id): "",
-    "{}.manifest ID".format(cnv_generate_bed_vep_stage_id): bioinformatic_manifest,
-    "{}.manifest".format(cnv_generate_bed_vep_stage_id): "",
     "{}.additional_regions ID".format(cnv_generate_bed_vep_stage_id): additional_regions,
     "{}.additional_regions".format(cnv_generate_bed_vep_stage_id): "",
+    # input for eggd_vep
+    "{}.config_file ID".format(cnv_vep_stage_id): cnv_vep_config,
+    "{}.config_file".format(cnv_vep_stage_id): "",
     # inputs for generate bed for excluded app
-    "{}.exons_nirvana ID".format(cnv_generate_bed_excluded_stage_id): cds_file,
+    "{}.exons_nirvana ID".format(cnv_generate_bed_excluded_stage_id): exons_nirvana,
     "{}.exons_nirvana".format(cnv_generate_bed_excluded_stage_id): "",
     "{}.nirvana_genes2transcripts ID".format(cnv_generate_bed_excluded_stage_id): genes2transcripts,
     "{}.nirvana_genes2transcripts".format(cnv_generate_bed_excluded_stage_id): "",
     "{}.gene_panels ID".format(cnv_generate_bed_excluded_stage_id): genepanels_file,
     "{}.gene_panels".format(cnv_generate_bed_excluded_stage_id): "",
-    "{}.manifest ID".format(cnv_generate_bed_excluded_stage_id): bioinformatic_manifest,
-    "{}.manifest".format(cnv_generate_bed_excluded_stage_id): "",
     "{}.additional_regions ID".format(cnv_generate_bed_excluded_stage_id): additional_regions,
     "{}.additional_regions".format(cnv_generate_bed_excluded_stage_id): "",
     # inputs for excluded app
-    "{}.cds_hgnc ID".format(cnv_annotate_excluded_regions_stage_id): cds_file,
+    "{}.cds_hgnc ID".format(cnv_annotate_excluded_regions_stage_id): exons_nirvana,
     "{}.cds_hgnc".format(cnv_annotate_excluded_regions_stage_id): "",
-    "{}.cds_gene ID".format(cnv_annotate_excluded_regions_stage_id): cds_file_for_athena,
+    "{}.cds_gene ID".format(cnv_annotate_excluded_regions_stage_id): exons_file,
     "{}.cds_gene".format(cnv_annotate_excluded_regions_stage_id): "",
     "{}.additional_regions ID".format(cnv_annotate_excluded_regions_stage_id): additional_regions,
     "{}.additional_regions".format(cnv_annotate_excluded_regions_stage_id): ""
 }
 
-# CNV reanalysis
-
-cnv_rea_stage_input_dict  = {
+# Sample-specific input files and their search patterns
+cnv_rpt_stage_input_dict = {
     # vep
     # subdirectories always require the backward dash
     "{}.vcf".format(cnv_vep_stage_id): {
@@ -181,33 +155,4 @@ cnv_rea_stage_input_dict  = {
         "app": "eggd_GATKgCNV_call", "subdir": "CNV_summary/",
         "pattern": "-E '(.*)_excluded_intervals.bed$'"
     },
-}
-cnv_rea_dynamic_files = {
-    # inputs for generate bed for vep
-    "{}.exons_nirvana ID".format(cnv_generate_bed_vep_stage_id): cds_file,
-    "{}.exons_nirvana".format(cnv_generate_bed_vep_stage_id): "",
-    "{}.nirvana_genes2transcripts ID".format(cnv_generate_bed_vep_stage_id): genes2transcripts,
-    "{}.nirvana_genes2transcripts".format(cnv_generate_bed_vep_stage_id): "",
-    "{}.gene_panels ID".format(cnv_generate_bed_vep_stage_id): genepanels_file,
-    "{}.gene_panels".format(cnv_generate_bed_vep_stage_id): "",
-    "{}.additional_regions ID".format(cnv_generate_bed_vep_stage_id): additional_regions,
-    "{}.additional_regions".format(cnv_generate_bed_vep_stage_id): "",
-# inputs for generate bed for excluded app
-    "{}.exons_nirvana ID".format(cnv_generate_bed_excluded_stage_id): cds_file,
-    "{}.exons_nirvana".format(cnv_generate_bed_excluded_stage_id): "",
-    "{}.nirvana_genes2transcripts ID".format(cnv_generate_bed_excluded_stage_id): genes2transcripts,
-    "{}.nirvana_genes2transcripts".format(cnv_generate_bed_excluded_stage_id): "",
-    "{}.gene_panels ID".format(cnv_generate_bed_excluded_stage_id): genepanels_file,
-    "{}.gene_panels".format(cnv_generate_bed_excluded_stage_id): "",
-    "{}.manifest ID".format(cnv_generate_bed_excluded_stage_id): bioinformatic_manifest,
-    "{}.manifest".format(cnv_generate_bed_excluded_stage_id): "",
-    "{}.additional_regions ID".format(cnv_generate_bed_excluded_stage_id): additional_regions,
-    "{}.additional_regions".format(cnv_generate_bed_excluded_stage_id): "",
-    # inputs for excluded app
-    "{}.cds_hgnc ID".format(cnv_annotate_excluded_regions_stage_id): cds_file,
-    "{}.cds_hgnc".format(cnv_annotate_excluded_regions_stage_id): "",
-    "{}.cds_gene ID".format(cnv_annotate_excluded_regions_stage_id): cds_file_for_athena,
-    "{}.cds_gene".format(cnv_annotate_excluded_regions_stage_id): "",
-    "{}.additional_regions ID".format(cnv_annotate_excluded_regions_stage_id): additional_regions,
-    "{}.additional_regions".format(cnv_annotate_excluded_regions_stage_id): ""
 }
