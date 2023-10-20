e8113a19d2493cf7a344ead328b7ea213f4b5177
Chris Pyatt <34276603+chrispyatt@users.noreply.github.com>
2023-10-20 10:15:38 +0100

    Update and rename egg5_config_v2.3.0.py to egg5_config_v2.3.1.py
    added gnomad exclude columns to reports and mosaic reports

--- egg5_config_v2.3.1.py
+++ egg5_config_v2.3.1.py
@@ -72,7 +72,7 @@ rpt_dynamic_files = {
     "{}.config_file ID".format(vep_stage_id): vep_config,
     "{}.config_file".format(vep_stage_id): "",
     # inputs for generate_variant_workbook
-    "{}.exclude_columns".format(generate_workbook_stage_id): "BaseQRankSum ClippingRankSum DB ExcessHet FS MLEAC MLEAF MQ MQRankSum QD ReadPosRankSum SOR PL QUAL ID FILTER  CSQ_ClinVar_CLNSIGCONF  CSQ_Allele CSQ_HGNC_ID DP AC AF AN CSQ_SpliceAI_pred_DP_AL CSQ_SpliceAI_pred_DP_AG CSQ_SpliceAI_pred_DP_DG CSQ_SpliceAI_pred_DP_DL",
+    "{}.exclude_columns".format(generate_workbook_stage_id): "BaseQRankSum ClippingRankSum DB ExcessHet FS MLEAC MLEAF MQ MQRankSum QD ReadPosRankSum SOR PL QUAL ID FILTER  CSQ_ClinVar_CLNSIGCONF  CSQ_Allele CSQ_HGNC_ID DP AC AF AN CSQ_SpliceAI_pred_DP_AL CSQ_SpliceAI_pred_DP_AG CSQ_SpliceAI_pred_DP_DG CSQ_SpliceAI_pred_DP_DL gnomADe_AC_popmax gnomADe_AF_popmax gnomADe_AN_popmax gnomADe_nhomalt gnomADe_nhomalt_popmax gnomADe_non_cancer_AC gnomADe_non_cancer_AC_popmax gnomADe_non_cancer_AF gnomADe_non_cancer_AF_popmax gnomADe_non_cancer_AN gnomADe_non_cancer_AN_popmax gnomADe_non_cancer_nhomalt gnomADe_non_cancer_nhomalt_popmax gnomADe_non_cancer_popmax gnomADe_popmax gnomADg_AC_popmax gnomADg_AF_popmax gnomADg_AN_popmax gnomADg_nhomalt gnomADg_nhomalt_popmax gnomADg_popmax",
     "{}.acmg".format(generate_workbook_stage_id): "true",
     "{}.rename_columns".format(generate_workbook_stage_id): "CSQ_Feature=Transcript DP_FMT=DP",
     "{}.add_comment_column".format(generate_workbook_stage_id): "true",
@@ -123,7 +123,7 @@ mosaic_rpt_dynamic_files = {
     "{}.config_file ID".format(vep_stage_id): vep_config,
     "{}.config_file".format(vep_stage_id): "",
     # inputs for generate_variant_workbook
-    "{}.exclude_columns".format(generate_workbook_stage_id): "BaseQRankSum ClippingRankSum DB ExcessHet FS MLEAC MLEAF MQ MQRankSum QD ReadPosRankSum SOR PL QUAL ID FILTER  CSQ_ClinVar_CLNSIGCONF  CSQ_Allele CSQ_HGNC_ID DP AC AF AN CSQ_SpliceAI_pred_DP_AL CSQ_SpliceAI_pred_DP_AG CSQ_SpliceAI_pred_DP_DG CSQ_SpliceAI_pred_DP_DL  AS_FilterStatus AS_SB_TABLE ECNT GERMQ MBQ MFRL MMQ MPOS POPAF ROQ TLOD F1R2 F2R1 SB",
+    "{}.exclude_columns".format(generate_workbook_stage_id): "BaseQRankSum ClippingRankSum DB ExcessHet FS MLEAC MLEAF MQ MQRankSum QD ReadPosRankSum SOR PL QUAL ID FILTER  CSQ_ClinVar_CLNSIGCONF  CSQ_Allele CSQ_HGNC_ID DP AC AF AN CSQ_SpliceAI_pred_DP_AL CSQ_SpliceAI_pred_DP_AG CSQ_SpliceAI_pred_DP_DG CSQ_SpliceAI_pred_DP_DL  AS_FilterStatus AS_SB_TABLE ECNT GERMQ MBQ MFRL MMQ MPOS POPAF ROQ TLOD F1R2 F2R1 SB gnomADe_AC_popmax gnomADe_AF_popmax gnomADe_AN_popmax gnomADe_nhomalt gnomADe_nhomalt_popmax gnomADe_non_cancer_AC gnomADe_non_cancer_AC_popmax gnomADe_non_cancer_AF gnomADe_non_cancer_AF_popmax gnomADe_non_cancer_AN gnomADe_non_cancer_AN_popmax gnomADe_non_cancer_nhomalt gnomADe_non_cancer_nhomalt_popmax gnomADe_non_cancer_popmax gnomADe_popmax gnomADg_AC_popmax gnomADg_AF_popmax gnomADg_AN_popmax gnomADg_nhomalt gnomADg_nhomalt_popmax gnomADg_popmax",
     "{}.acmg".format(generate_workbook_stage_id): "true",
     "{}.rename_columns".format(generate_workbook_stage_id): "CSQ_Feature=Transcript DP_FMT=DP",
     "{}.add_comment_column".format(generate_workbook_stage_id): "true",
@@ -207,7 +207,7 @@ cnv_rpt_dynamic_files = {
     "{}.additional_regions".format(cnv_annotate_excluded_regions_stage_id): "",
     # inputs for generate variant workbook
     "{}.additional_sheet_names".format(cnv_generate_workbook_stage_id): "ExcludedRegions",
-    "{}.exclude_columns".format(cnv_generate_workbook_stage_id): "REF FILTER CSQ_Allele CSQ_Consequence CSQ_IMPACT",
+    "{}.exclude_columns".format(cnv_generate_workbook_stage_id): "REF FILTER CSQ_Allele CSQ_Consequence CSQ_IMPACT ",
     "{}.acmg".format(cnv_generate_workbook_stage_id): "true",
     "{}.reorder_columns".format(cnv_generate_workbook_stage_id): "CHROM POS END CNVLEN ID ALT QUAL CSQ_SYMBOL CSQ_Feature CSQ_VARIANT_CLASS CSQ_EXON CSQ_INTRON CSQ_STRAND GT CN NP QA QS QSE QSS",
     "{}.add_comment_column".format(cnv_generate_workbook_stage_id): "true",
